8e40974a56a90b7c2b301142a91aacecab5a5873
Sencyy <sencyy@protonmail.com>
2026-08-15 20:28:45 -0300

    fix logging path

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,14 @@
 import logging
 from logging import Logger
 from os import remove
+from pathlib import Path
 class Log:
     log: Logger
     def __init__(self):
-        remove("execution.log")
+        if Path("../execution.log").exists():
+            remove("../execution.log")
         self.log = logging.getLogger(__name__)
-        logging.basicConfig(filename='execution.log', format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+        logging.basicConfig(filename='../execution.log', format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
 
     def loginfo(self, caller: str,  message: str):
         self.log.info(f"Admin {caller.upper()}: {message}")
